bceb089ece1fc9f2648723a7b4deb9fda18eaee0
Nimra Aftab <nimra.aftab@mail.utoronto.ca>
2022-06-15 19:36:40 -0400

    fix merge error

--- week1/helloworld.js
+++ week1/helloworld.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 function hello(){
     return "Hello World!"
 }
 module.exports = hello
-=======
-export function hello(){
-    return "Hello World";
-}
->>>>>>> f197f2b76a37fde6856974f9adce5afa10e26957
