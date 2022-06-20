a30e2e86ed771b1d62da6042fb8f068c8eec1f18
Nimra Aftab <nimra.aftab@mail.utoronto.ca>
2022-06-20 19:09:43 -0400

    move code to index.js

--- twocatz/index.js
+++ twocatz/index.js
@@ -2,30 +2,26 @@ const fetch = require('node-fetch')
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    const resp = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
-        method: 'GET'
-    });
-    const data = await resp.arrayBuffer()
-    var base64data = Buffer.from(data).toString('base64')
     
-    const resp2 = await fetch("https://bit-cat.azurewebsites.net/cat/says/serverless", {
-        method: 'GET'
-    });
-    const data2 = await resp.arrayBuffer()
-    var base64data2 = Buffer.from(data2).toString('base64')
-    let all_names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
-   
-    function result_name(names){
-        let random_value = Math.floor(Math.random() * all_names.length)
-        return names[random_value]
+    async function getPic(){
+        const resp = await fetch("https://cataas.com/cat/cute/says/Bitcamp", {method: 'GET'});
+        const data = await resp.arrayBuffer()
+        return Buffer.from(data).toString('base64')
     }
+   
+    function result_name(){
+        var all_names = ["Shreya", "Emily", "Fifi", "Beau", "Evelyn", "Julia", "Daniel", "Fardeen"]
+        let random_value = Math.floor(Math.random() * all_names.length)
+        return all_names[random_value]
+    }
+    var firstCat = await getPic();
+    var secondCat = await getPic();
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {
-            cat1: {base64data}, 
-            cat2: {base64data2},
-            names: [result_name(all_names), result_name(all_names)]
+            cat1: {firstCat}, 
+            cat2: {secondCat},
+            names: [result_name(), result_name()]
         }
     };
 }
